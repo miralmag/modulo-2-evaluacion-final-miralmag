9ac70a8e5bfe832730ccd4ed7784a00393b8d916
miralmag <miriam.almagro@gmail.com>
2022-07-15 18:08:38 +0200

    add fetch to server

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('>> Ready :)');
+// Variables globales
+
+const textInput = document.querySelector('.js_text_input');
+const searchButton = document.querySelector('.js_search_btn');
+const resetButton = document.querySelector('.js_reset_btn');
+const ulAnimes = document.querySelector('.js_ul_list');
+let animeList = [];
+
+// Funciones
+
+
+
+
+
+// Petición al servidor
+fetch ("https://api.jikan.moe/v4/anime")
+.then ((response) => response.json())
+.then ((data) => {
+    animeList = data.data;
+    ulAnimes.innerHTML = `<li><img src=${animeList[0].images.jpg.image_url}><h3>${animeList[1].title}</h3></li>`;
+})
+
+// Acciones al cargar la página
