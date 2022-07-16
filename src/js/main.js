0c5af946b555fd8413ddc04e8be3752a7d7cdccc
miralmag <miriam.almagro@gmail.com>
2022-07-16 13:19:34 +0200

    add render favourites functionality

--- src/js/main.js
+++ src/js/main.js
@@ -6,21 +6,37 @@ const textInput = document.querySelector('.js_text_input');
 const searchButton = document.querySelector('.js_search_btn');
 const resetButton = document.querySelector('.js_reset_btn');
 const ulAnimes = document.querySelector('.js_ul_list');
+const ulFavouriteAnimes = document.querySelector('.js_favourites_list');
 let animeList = [];
 let favouriteAnimeList = [];
 
 // Funciones
 
 function renderFavourites() {
-    
+  let html = '';
+  for (const eachfav of favouriteAnimeList) {
+    if (eachfav.images.jpg.image_url !== 'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png') {html += `<li class="js_anime_item" id="${eachfav.mal_id}"><img src=${eachfav.images.jpg.image_url}><h3>${eachfav.title}</h3></li>`;
+    } else {
+      html += `<li class="js_anime_item" id="${eachfav.mal_id}"><img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"><h3>${eachfav.title}</h3></li>`;
+    }
+    ulFavouriteAnimes.innerHTML = html;
+  }
 }
 
+
 function handleClickFavourites(event){
-    console.log(event.currentTarget.id);
-    const selectedItem = event.currentTarget.id;
-    favouriteAnimeList.push(selectedItem);
-    console.log(favouriteAnimeList);
-    renderFavourites();
+  const selectedId = parseInt(event.currentTarget.id);
+
+  const animeSelected = animeList.find((anime) => anime.mal_id === selectedId);
+  const favouriteAnimeSelected = favouriteAnimeList.findIndex((fav) => fav.mal_id === selectedId);
+
+  if (favouriteAnimeSelected === -1) {
+    favouriteAnimeList.push(animeSelected);
+  } else {favouriteAnimeList.splice(favouriteAnimeSelected, 1);
+  }
+  console.log(favouriteAnimeList);
+  renderFavourites();
+  listenerFavourites();
 }
 
 function listenerFavourites() {
@@ -33,7 +49,7 @@ function listenerFavourites() {
 function renderAnimes(){
   let html = '';
   for (const eachAnime of animeList) {
-    if (eachAnime.images.jpg.image_url !== "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png") {html += `<li class="js_anime_item" id="${eachAnime.mal_id}"><img src=${eachAnime.images.jpg.image_url}><h3>${eachAnime.title}</h3></li>`;
+    if (eachAnime.images.jpg.image_url !== 'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png') {html += `<li class="js_anime_item" id="${eachAnime.mal_id}"><img src=${eachAnime.images.jpg.image_url}><h3>${eachAnime.title}</h3></li>`;
     } else {
       html += `<li class="js_anime_item" id="${eachAnime.mal_id}"><img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"><h3>${eachAnime.title}</h3></li>`;
     }
@@ -50,7 +66,7 @@ function handleClick(event) {
     .then ((data) => {
       animeList = data.data;
       renderAnimes();
-    })
+    });
 }
 
 // Eventos
